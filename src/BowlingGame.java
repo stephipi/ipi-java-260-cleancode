522cd1f06972fdcfa9114438e900cf11ff9f3331
stephipi <stephane.ripe@ipilyon.net>
2018-04-22 21:26:10 +0200

    ajout des tests pour spare / strike / sparePasStrike

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,12 +1,54 @@
 public class BowlingGame {
 
     private int score;
+    
+    private int frame;
+    private int nbRoll=0;
 
+    private int bonusPoint;
+    
     public int score() {
         return score;
     }
-
+    
     public void roll(int numberPins) {
-        score += numberPins;
+    		nbRoll += 1;
+    		frame += numberPins;
+    		
+    		if (bonusPoint != 0)
+    		{
+    			frame += numberPins;
+    			bonusPoint -=1;
+    		}
+    			
+    		addStrikeBonus(numberPins, nbRoll);
+    		
+    		addSpareBonus(frame);
+			
+    		if (nbRoll == 2)
+    		{
+    			updateScore(frame);
+    		}
+    }
+    
+    private void updateScore(int localFrame)
+    {
+    	score += localFrame;
+		frame = 0;
+		nbRoll = 0;
+    }
+    
+    private int addStrikeBonus(int numberPins,int nbRoll)
+    {
+    	if ((numberPins == 10) && (nbRoll == 1))
+			bonusPoint += 1;
+    	return bonusPoint;
+    }
+    
+    private int addSpareBonus(int frame)
+    {
+    	if (frame == 10)
+			bonusPoint += 1;
+    	return bonusPoint;
     }
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -8,25 +8,44 @@ public class BowlingGameTest {
     BowlingGame game = new BowlingGame();
 
     @Test
-    public void queDesGoutieresDonne0Points() {
+    public void testQueDesGoutieresDonne0Points() {
         rollMany(20, 0);
         assertEquals(0, game.score());
     }
 
     @Test
-    public void uneSeuleQuille(){
+    public void testUneSeuleQuille(){
         game.roll(1);
         rollMany(19, 0);
         assertEquals(1, game.score());
     }
 
     @Test
-    public void bonusDuSpare(){
+    public void testBonusDuSpare(){
         game.roll(8);
         game.roll(2);
         game.roll(1);
         rollMany(17, 0);
         assertEquals(12, game.score());
+    }
+
+    @Test
+    public void testBonusDuStrike(){
+        game.roll(10);
+        game.roll(2);
+        game.roll(1);
+        rollMany(16, 0);
+        assertEquals(16, game.score());
+    }
+
+    @Test
+    public void testBonusDuSparePasStrike(){
+        game.roll(0);
+        game.roll(10);
+        game.roll(2);
+        game.roll(5);
+        rollMany(16, 0);
+        assertEquals(19, game.score());
     }
 
     private void rollMany(int numberRolls, int numberPins) {
